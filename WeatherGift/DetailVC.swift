3766c46a8d9fef10db263e89701ad4605ddb863f
Christopher Donnelly <christopherdonnelly@christosmacbook.bc.edu>
2019-03-11 09:28:39 -0400

    7.3 Complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Christopher Donnelly on 3/6/19.
+//  Copyright © 2019 Chris Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationLabel.text = locationsArray[currentPage]
+    }
+
+}
---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Christopher Donnelly on 3/10/19.
+//  Copyright © 2019 Chris Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
