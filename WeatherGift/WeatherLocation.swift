ecd7cb3764bf6c2c1de76155119813ca6eb38b88
Christopher Donnelly <christopherdonnelly@christophers-macbook.local>
2019-03-17 15:31:11 -0400

    7.11 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Christopher Donnelly on 3/17/19.
+//  Copyright © 2019 Chris Donnelly. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
